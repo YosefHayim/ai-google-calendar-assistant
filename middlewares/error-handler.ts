28a21fabb8b1db2c98baed72f1bbffcb90ce52e0
YosefHayim <yosefhayimsabag@gmail.com>
2025-06-29 14:23:52 +0300

    refine agian the package json

--- middlewares/error-handler.ts
+++ middlewares/error-handler.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-const errorHandler = (err: Error, req: Request, res: Response) => {
+const errorHandler = (err: any, req: Request, res: Response) => {
   console.error(err.stack);
   const status = err.status || 500;
   res.status(status).json({ error: err.message || 'Internal Server Error' });
