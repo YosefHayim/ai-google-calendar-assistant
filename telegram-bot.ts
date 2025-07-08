3761129831e9aad40e7705dda8f9edeaa25a7972
YosefHayim <yosefhayimsabag@gmail.com>
2025-07-08 21:16:15 +0300

    working on telegram bot

--- telegram-bot.ts
+++ telegram-bot.ts
@@ -3,9 +3,15 @@ import { CONFIG } from "./config/root-config";
 
 const bot = new Bot(CONFIG.telegram_access_token!);
 
-// Handle the /start command.
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-// Handle other messages.
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+const initBot = async () => {
+  // Handle the /start command.
+  bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+  // Handle other messages.
+  bot.on("message", (ctx) => ctx.reply("Got another message!"));
 
-bot.start();
+  bot.start();
+
+  await bot.api.sendMessage(12345, "Hi!");
+};
+
+initBot();
