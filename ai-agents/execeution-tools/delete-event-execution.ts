2f5006b9c981837c79af760a95331d5ebfaa735e
YosefHayim <yosefhayimsabag@gmail.com>
2025-07-22 08:00:53 +0300

    error handle for insert and delete event

--- ai-agents/execeution-tools/delete-event-execution.ts
+++ ai-agents/execeution-tools/delete-event-execution.ts
@@ -7,23 +7,23 @@ import { handleEvents } from "../../utils/handler-calendar-event";
 export const deleteEventExecution = asyncHandler(async (params: any) => {
   console.log("Params received to delete event tool:", params);
 
-  if (!params.start?.dateTime || !params.end?.dateTime) {
-    errorTemplate("Missing dates of start and end!", 404);
-  }
+  if (!params.start?.dateTime || !params.end?.dateTime) errorTemplate("Missing dates of start and end!", 404);
+  if (!params.summary) errorTemplate("Missing event summary!", 404);
+  if (params.start.timeZone !== params.end.timeZone) errorTemplate("Time zones must match!", 404);
 
   const startDate = new Date(params.start.dateTime);
   const endDate = params.end ? new Date(params.end.dateTime) : new Date(startDate.getTime() + 60 * 60 * 1000);
 
   const eventData: calendar_v3.Schema$Event = {
     summary: params.summary,
-    description: params.description ?? "",
+    description: params.description,
     start: {
       dateTime: startDate.toISOString(),
       timeZone: params.start.timeZone,
     },
     end: {
       dateTime: endDate.toISOString(),
-      timeZone: params.end?.timeZone || params.start.timeZone,
+      timeZone: params.end.timeZone,
     },
   };
 
--- ai-agents/execeution-tools/insert-event-execution.ts
+++ ai-agents/execeution-tools/insert-event-execution.ts
@@ -1,23 +1,28 @@
 import { Action } from "../../types";
 import { calendar_v3 } from "googleapis";
+import errorTemplate from "../../utils/error-template";
 import { handleEvents } from "../../utils/handler-calendar-event";
 
 export const insertEventExeuction = async (params: any) => {
   console.log("Params received to insert event tool:", params);
+
+  if (!params.start?.dateTime || !params.end?.dateTime) errorTemplate("Missing dates of start and end!", 404);
+  if (!params.summary) errorTemplate("Missing event summary!", 404);
+  if (params.start.timeZone !== params.end.timeZone) errorTemplate("Time zones must match!", 404);
 
   const startDate = new Date(params.start.dateTime);
   const endDate = params.end ? new Date(params.end.dateTime) : new Date(startDate.getTime() + 60 * 60 * 1000);
 
   const eventData: calendar_v3.Schema$Event = {
     summary: params.summary,
-    description: params.description ?? "",
+    description: params.description,
     start: {
       dateTime: startDate.toISOString(),
       timeZone: params.start.timeZone,
     },
     end: {
       dateTime: endDate.toISOString(),
-      timeZone: params.end?.timeZone || params.start.timeZone,
+      timeZone: params.end?.timeZone,
     },
   };
 
