bea5341ed7beb9b40c4f75b6a69f20e321402b59
YosefHayim <yosefhayimsabag@gmail.com>
2025-08-03 02:10:52 +0300

    d

--- controllers/users-controller.ts
+++ controllers/users-controller.ts
@@ -1,5 +1,3 @@
-//@ts-nocheck
-
 import { CONFIG, OAUTH2CLIENT, SCOPES, SCOPES_STRING, SUPABASE } from "../config/root-config";
 import { PROVIDERS, STATUS_RESPONSE } from "../types";
 import { Request, Response } from "express";
@@ -24,7 +22,7 @@ const generateAuthGoogleUrl = reqResAsyncHandler(async (req, res) => {
   // 1. No code yet: send user to consent screen
   if (!code) {
     // If from Postman, just send the URL back instead of redirecting
-    if (postmanHeaders?.includes("Postman")) return sendR(STATUS_RESPONSE.SUCCESS, url);
+    if (postmanHeaders?.includes("Postman")) return sendR(res, STATUS_RESPONSE.SUCCESS, url);
     return res.redirect(url);
   }
 
@@ -35,31 +33,33 @@ const generateAuthGoogleUrl = reqResAsyncHandler(async (req, res) => {
     // 3. Token still valid
     OAUTH2CLIENT.setCredentials(tokens);
 
-    const user = jwt.decode(tokens.id_token);
+    const user = jwt.decode(tokens.id_token!);
 
     const { data, error } = await SUPABASE.from("calendars_of_users")
       .insert({
         ...tokens,
         scope: SCOPES.join(" "),
+        //@ts-ignore
         email: user.email,
       })
+      //@ts-ignore
       .eq("email", user.email)
       .select();
 
     if (error) {
       console.error("Error inserting tokens into database:", error);
-      return sendR(STATUS_RESPONSE.INTERNAL_SERVER_ERROR, "Failed to store new tokens.", error);
+      return sendR(res, STATUS_RESPONSE.INTERNAL_SERVER_ERROR, "Failed to store new tokens.", error);
     }
 
-    sendR(STATUS_RESPONSE.SUCCESS, "Existing token is still valid.", { data });
+    sendR(res, STATUS_RESPONSE.SUCCESS, "Existing token is still valid.", { data });
   } catch (error) {
     console.error("generateAuthUrl error:", error);
-    sendR(STATUS_RESPONSE.INTERNAL_SERVER_ERROR, "Failed to process OAuth token exchange.", error);
+    sendR(res, STATUS_RESPONSE.INTERNAL_SERVER_ERROR, "Failed to process OAuth token exchange.", error);
   }
 });
 
 const signUpUserReg = asyncHandler(async (req: Request, res: Response) => {
-  if (!req.body.email || !req.body.password) sendR(STATUS_RESPONSE.BAD_REQUEST, "Email and password are required.");
+  if (!req.body.email || !req.body.password) sendR(res, STATUS_RESPONSE.BAD_REQUEST, "Email and password are required.");
   console.log(req.body);
   const { data, error } = await SUPABASE.auth.signUp({
     email: req.body.email,
@@ -68,10 +68,10 @@ const signUpUserReg = asyncHandler(async (req: Request, res: Response) => {
 
   if (error) {
     console.error("Error signing up regularly user:", error);
-    sendR(STATUS_RESPONSE.INTERNAL_SERVER_ERROR, "Failed to sign up user.", error);
+    sendR(res, STATUS_RESPONSE.INTERNAL_SERVER_ERROR, "Failed to sign up user.", error);
   }
   if (data) {
-    sendR(STATUS_RESPONSE.SUCCESS, "User signed up successfully.", data);
+    sendR(res, STATUS_RESPONSE.SUCCESS, "User signed up successfully.", data);
   }
 });
 
@@ -86,14 +86,14 @@ const signUpUserViaGitHub = asyncHandler(async (req: Request, res: Response) => 
 });
 
 const getUserInformation = asyncHandler(async (req, res) => {
-  if (!req.user) return sendR(STATUS_RESPONSE.UNAUTHORIZED, "User not authenticated.");
-  sendR(STATUS_RESPONSE.SUCCESS, "User fetched successfully.", req.user);
+  if (!req.user) return sendR(res, STATUS_RESPONSE.UNAUTHORIZED, "User not authenticated.");
+  sendR(res, STATUS_RESPONSE.SUCCESS, "User fetched successfully.", req.user);
 });
 
 const deActivateUser = asyncHandler(async (req: Request, res: Response) => {});
 
 const signInUserReg = asyncHandler(async (req: Request, res: Response) => {
-  if (!req.body.email || !req.body.password) sendR(STATUS_RESPONSE.BAD_REQUEST, "Email and password are required ");
+  if (!req.body.email || !req.body.password) sendR(res, STATUS_RESPONSE.BAD_REQUEST, "Email and password are required ");
 
   const { data, error } = await SUPABASE.auth.signInWithPassword({
     email: req.body.email,
@@ -102,10 +102,10 @@ const signInUserReg = asyncHandler(async (req: Request, res: Response) => {
 
   if (error) {
     console.error("Error signing up regularly user by email:", error);
-    sendR(STATUS_RESPONSE.INTERNAL_SERVER_ERROR, "Failed to fetch user by email.", error);
+    sendR(res, STATUS_RESPONSE.INTERNAL_SERVER_ERROR, "Failed to fetch user by email.", error);
   }
 
-  sendR(STATUS_RESPONSE.SUCCESS, "User signin successfully.", data);
+  sendR(res, STATUS_RESPONSE.SUCCESS, "User signin successfully.", data);
 });
 
 const updateUserById = asyncHandler(async (req: Request, res: Response) => {});
