91ac9ce855304b7da9fc10335968e14fcd2e7eb3
YosefHayim <yosefhayimsabag@gmail.com>
2025-08-15 14:10:50 +0300

    reset conversations on tg recreating

--- telegram-bot/init-bot.ts
+++ telegram-bot/init-bot.ts
@@ -1,7 +1,6 @@
 import { Bot, session, SessionFlavor, type Context } from "grammy";
 import { conversations, createConversation, type ConversationFlavor } from "@grammyjs/conversations";
 import { MenuFlavor } from "@grammyjs/menu";
-import { insertEventToCalendar, searchForEventByName, deleteEventByName, getCalendarList, updateEventByName, chatWithAgent } from "./conversations";
 
 import { CONFIG } from "@/config/root-config";
 import { mainMenu } from "./menus";
@@ -25,12 +24,6 @@ function initialSession(): SessionData {
 bot.use(session({ initial: initialSession }));
 bot.use(authTgHandler);
 bot.use(conversations());
-bot.use(createConversation(insertEventToCalendar));
-bot.use(createConversation(searchForEventByName));
-bot.use(createConversation(deleteEventByName));
-bot.use(createConversation(updateEventByName));
-bot.use(createConversation(getCalendarList));
-bot.use(createConversation(chatWithAgent));
 bot.use(mainMenu);
 
 bot.command("start", async (ctx) => {
