3dfd74bf09afbcbbbdab5e3e81e7454b846fb20c
YosefHayim <yosefhayimsabag@gmail.com>
2025-09-06 19:40:45 +0300

    .

--- utils/update-calendar-categories.ts
+++ utils/update-calendar-categories.ts
@@ -1,15 +1,11 @@
 import { SUPABASE } from '@/config/root-config';
-import type { TablesInsert } from '@/database.types';
+import type { Tables, TablesInsert } from '@/database.types';
 import type { UpdateCalendarCategoriesProps } from '@/types';
 import { asyncHandler } from './async-handlers';
 
 export const updateCalenderCategories = asyncHandler(
-  async (
-    payload: UpdateCalendarCategoriesProps[] | undefined,
-    email: string,
-    userId: string | null
-  ): Promise<TablesInsert<'calendar_categories'>[] | false> => {
-    if (payload?.length === 0 || !payload || !email) {
+  async (payload: UpdateCalendarCategoriesProps[] | undefined, email: string, userId: string | null): Promise<Tables<'calendar_categories'>[] | false> => {
+    if (!(payload?.length && email)) {
       return false;
     }
 
@@ -17,23 +13,24 @@ export const updateCalenderCategories = asyncHandler(
       .filter((p) => p?.calendarId)
       .map((p) => ({
         access_role: p.accessRole ?? null,
-        calendar_id: p.calendarId,
+        calendar_id: p.calendarId as string,
         calendar_name: p.calendarName ?? null,
         email,
-        user_id: userId,
         time_zone_of_calendar: p.timeZoneForCalendar ?? null,
-      }));
+        default_reminders: p.defaultReminders ?? null,
+        updated_at: new Date().toISOString(),
+        user_id: userId,
+      })) as TablesInsert<'calendar_categories'>[];
 
     if (!rows.length) {
       return false;
     }
-    const { data, error } = await SUPABASE.from('calendar_categories')
-      .upsert({ ...rows, updated_at: new Date().toISOString() })
-      .select();
+
+    const { data, error } = await SUPABASE.from('calendar_categories').upsert(rows).select();
 
     if (error) {
       throw error;
     }
-    return data;
+    return data ?? false;
   }
 );
