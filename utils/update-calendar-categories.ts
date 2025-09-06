0d86b05c8e43e9178afd80e01e624a7b88b25f8f
YosefHayim <yosefhayimsabag@gmail.com>
2025-09-06 19:33:44 +0300

    .

--- utils/update-calendar-categories.ts
+++ utils/update-calendar-categories.ts
@@ -1,34 +1,39 @@
 import { SUPABASE } from '@/config/root-config';
+import type { TablesInsert } from '@/database.types';
 import type { UpdateCalendarCategoriesProps } from '@/types';
 import { asyncHandler } from './async-handlers';
 
-export const updateCalenderCategories = asyncHandler(async (payload: UpdateCalendarCategoriesProps[] | undefined, email: string, userId: string | null) => {
-  if (payload?.length === 0 || !payload || !email) {
-    return false;
+export const updateCalenderCategories = asyncHandler(
+  async (
+    payload: UpdateCalendarCategoriesProps[] | undefined,
+    email: string,
+    userId: string | null
+  ): Promise<TablesInsert<'calendar_categories'>[] | false> => {
+    if (payload?.length === 0 || !payload || !email) {
+      return false;
+    }
+
+    const rows = payload
+      .filter((p) => p?.calendarId)
+      .map((p) => ({
+        access_role: p.accessRole ?? null,
+        calendar_id: p.calendarId,
+        calendar_name: p.calendarName ?? null,
+        email,
+        user_id: userId,
+        time_zone_of_calendar: p.timeZoneForCalendar ?? null,
+      }));
+
+    if (!rows.length) {
+      return false;
+    }
+    const { data, error } = await SUPABASE.from('calendar_categories')
+      .upsert({ ...rows, updated_at: new Date().toISOString() })
+      .select();
+
+    if (error) {
+      throw error;
+    }
+    return data;
   }
-
-  const nowIso = new Date().toISOString();
-
-  const rows = payload
-    .filter((p) => p?.calendarId)
-    .map((p) => ({
-      access_role: p.accessRole ?? null,
-      calendar_id: p.calendarId,
-      calendar_name: p.calendarName ?? null,
-      email,
-      time_zone_of_calendar: p.timeZoneForCalendar ?? null,
-      updated_at: nowIso,
-    }));
-
-  if (!rows.length) {
-    return false;
-  }
-
-  const { error } = await SUPABASE.from('calendar_categories').upsert({ ...rows, user_id: userId });
-
-  if (error) {
-    throw error;
-  }
-
-  return true;
-});
+);
